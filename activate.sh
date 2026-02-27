5a89739bfab9e73e55589dfd195771cf09b7a4c4
Paolo Cozzi <bunop@libero.it>
2026-02-27 12:48:22 +0100

    :sparkles: Add test_evqa.py, update .gitignore, and modify setup_and_inference files

--- activate.sh
+++ activate.sh
@@ -2,4 +2,10 @@
 module purge
 module load python
 module load cuda
+
+export HF_HUB_CACHE="./MLLM_challenge/hf_models"
+export HF_HOME="./MLLM_challenge/hf_models"
+export TRANSFORMERS_OFFLINE="1"
+export HF_HUB_OFFLINE="1"
+
 source MLLM_challenge/pyenvs/MLLM_challenge/bin/activate
